740eeb5494f290ea28e2203fce1e1adf66e6c20f
YGears <yourivandegeer123@gmail.com>
2024-12-29 10:00:43 +0100

    Added PyQt5 as requirement

--- Config.py
+++ Config.py
@@ -2,9 +2,12 @@ import subprocess
 import sys
 
 def install_requirements():
+    #install requirements
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-    print("installed packages from requirements.txt")
-
+    
+    #list requirements
+    print("\nInstalled requirements: \n=======================\n")
+    subprocess.check_call([sys.executable, "-m", "pip", "list"])
 
 
 def call_functions():
