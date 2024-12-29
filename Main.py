a2b49f1647524116a7dbe29c0fb468c60e91f8bd
YGears <yourivandegeer123@gmail.com>
2024-12-29 10:10:42 +0100

    Added main boilerfile, added __pychache__ to .gitignore

---
+++ Main.py
@@ -0,0 +1,23 @@
+import Config
+"""
+The main python file, 
+from where our dreams come true and tears will be fed to the error in the terminal.
+
+Used to run all classes and their relevant functions.
+Objects will mostly be instantiated within Main.py, 
+unless there is a dependency within the called class, in which case it will call the class itself.
+
+Some files will end with the following code, in which case the code from the file will be ran instantly.
+def call_functions():
+    func_1()
+    func_2()
+    #..
+    func_n()
+call_functions()
+"""
+
+def Main():
+    
+    pass
+
+Main()
