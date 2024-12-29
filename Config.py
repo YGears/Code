de9730a1e45e0a319fc3443ceba31eb41d983b12
YGears <yourivandegeer123@gmail.com>
2024-12-29 09:52:30 +0100

    first_reset commit

--- Config.py
+++ Config.py
@@ -5,14 +5,10 @@ def install_requirements():
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
     print("installed packages from requirements.txt")
 
-def create_txt_installed_packages():
-    subprocess.check_call([sys.executable, "-m", "pip", "list", ">", ".installed_packages"])
-    print("created file .installed_packages")
 
 
 def call_functions():
     install_requirements()
-    create_txt_installed_packages()
 call_functions()
 
 
