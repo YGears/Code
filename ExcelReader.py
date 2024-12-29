00ae4f8dd6170d5579883dc0a714969ea33197c2
YGears <yourivandegeer123@gmail.com>
2024-12-29 12:20:42 +0100

    added get_vendor(), still needs work.

--- ExcelReader.py
+++ ExcelReader.py
@@ -9,9 +9,18 @@ class ExcelReader():
         return self.dataframe
 
     def read_file_to_dataframe(self, path:str):
-        dataframe = pd.read_excel(path, index_col=1)
+        dataframe = pd.read_excel(path, index_col='Rentedatum')
         self.dataframe = dataframe
         return dataframe
+    
+    def get_vendor(self):
+        data = self.dataframe['Omschrijving']
+        vendor_column = data.apply(str.split)
+        print(vendor_column)
+
+    def split_str_column(self, row):
+        return row.split()
+
     
 er =  ExcelReader()
 
@@ -20,3 +29,7 @@ with open('filepaths.txt', 'r') as file:
     path = file.read().replace('\n', '')
     
 data = er.read_file_to_dataframe(path)
+print(data)
+er.get_vendor()
+
+
