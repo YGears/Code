5e67d60cded9059eabb301c7be07894dbc95195d
Yourisogeti <youri.vande.geer@sogeti.com>
2025-01-07 13:54:23 +0100

    Added vendor parsing for bea, betaalpas

--- ExcelReader.py
+++ ExcelReader.py
@@ -51,7 +51,7 @@ class ExcelReader():
     def get_index_of_list(self, row:str, index:int=0):
         return row[index]
     
-    def get_temp_splitted_omschrijving_column(self):
+    def get_temp_splitted_omschrijving_column(self):    
         df = self.dataframe
         df['temp_splitted_omschrijving'] = df['Omschrijving'].apply(self.split_str_on_whitespace)
 
@@ -60,35 +60,49 @@ class ExcelReader():
 
         # df['PaymentType'] = df['Omschrijving'].apply(self.split_str_on_whitespace).apply(self.get_index_of_list, args=(0,))
 
-        df['PaymentType'] = df['temp_splitted_omschrijving'].apply(self.get_index_of_list, args=(0,))
+        self.dataframe['PaymentType'] = df['temp_splitted_omschrijving'].apply(self.get_index_of_list, args=(0,))
 
-        print(df.groupby(['PaymentType'], as_index=False)['PaymentType'].value_counts().sort_values('count').to_string())
+        # print(df.groupby(['PaymentType'], as_index=False)['PaymentType'].value_counts().sort_values('count').to_string())
 
-    def get_vendor_on_paymentmethod(self, row:pd.Series):
-    
+    def get_vendor_on_paymentmethod(self, row):
+        # print(type(row))
+        # print(row)
         payment_method:str = row['PaymentType']
 
         match payment_method:
             
             case "BEA, Betaalpas":
-                self.dataframe['Vendor'] = self.dataframe['temp_splitted_omschrijving'].apply(self.get_vendor_with_prefix_BEA_betaalpas)
-                return 
-            
+                # self.dataframe['Vendor'] = self.dataframe['temp_splitted_omschrijving'].apply(self.get_vendor_with_prefix_BEA_betaalpas)
+                print('Found BEA, Betaalpas')
+                print(row['temp_splitted_omschrijving'][1])
+                vendor_with_cardnumber = row['temp_splitted_omschrijving'][1]
+                vendor = vendor_with_cardnumber.split(',')[0]
+                print(vendor)
+                return vendor
+
             case "SEPA Overboeking":
+                print('Found SEPA Overboeking')
+
                 return
             
             case "SEPA iDEAL":
+                print('Found SEPA iDEAL')
+
+                return
+            
+            case "RENTE EN/OF KOSTEN":
+                print('Found RENTE EN/OF KOSTEN')
                 return
             
             #wildcard on all SEPA Incasso algemeen doorlopend
-            case ['SEPA Incasso algemeen doorlopend', *_]:
+            case x if x.startswith('SEPA Incasso algemeen doorlopend'):
+                print('Found SEPA Incasso algemeen doorlopend')
                 return
+            
+            case _:
+                print(f'Found else: {payment_method}')
 
-        
 
-
-        
-        pass
 
     def get_vendor_with_prefix_BEA_betaalpas(self):
         pass
@@ -137,8 +151,9 @@ class ExcelReader():
         self.dataframe['Omschrijving'] = self.dataframe['Omschrijving'].apply(lambda x : ",".join(re.split(r'\s{'+str(3)+',}', x)))
 
     def run(self):
-        er.get_temp_splitted_omschrijving_column()
-        er.get_paymenttype_column()
+        self.get_temp_splitted_omschrijving_column()
+        self.get_paymenttype_column()
+        self.dataframe['Vendor'] = self.dataframe.apply(self.get_vendor_on_paymentmethod, axis='columns')
 
         
 
@@ -152,15 +167,18 @@ er =  ExcelReader(path)
 
 
 
-df = er.get_dataframe()
+# df = er.get_dataframe()
 # er.clean_omschrijving()
 
 # df = df[df['Omschrijving'].str.startswith('BEA, Betaalpas')]
-# df = df[df['Omschrijving'].str.startswith('SEPA Overboeking')]
-# df = df[df['Omschrijving'].str.startswith('SEPA Incasso algemeen doorlopend Incassant: NL03ZZZ301243580000')]
+# # df = df[df['Omschrijving'].str.startswith('SEPA Overboeking')]
+# # df = df[df['Omschrijving'].str.startswith('SEPA Incasso algemeen doorlopend Incassant: NL03ZZZ301243580000')]
 # print(df.head(10).to_string(justify='left').replace('\n', '\n\n'))
 
 # print(er.get_omschrijving_when_vendor_nan())
 
 
 er.run()
+
+print(er.dataframe.loc[er.dataframe['Vendor'].notnull()], ['Vendor'])
+print(er.dataframe.loc[er.dataframe['Vendor'].notnull()], ['Vendor'].count)
